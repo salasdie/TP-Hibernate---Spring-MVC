3b57da524577e689af43665ae22162de1f3fe7ff
Diego salas <diegonsalas@gmail.com>
2019-04-20 22:21:51 -0300

    creacion de clases

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
@@ -0,0 +1,24 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.*;
+
+@Entity
+public class Ciudad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Ubicacion ubicacionGeografica;
+    @ManyToOne
+    private Pais pais;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public Ubicacion getUbicacionGeografica() { return ubicacionGeografica; }
+    public void setUbicacionGeografica(Ubicacion ubicacionGeografica) { this.ubicacionGeografica = ubicacionGeografica; }
+
+    public Pais getPais() { return pais; }
+    public void setPais(Pais pais) { this.pais = pais; }
+}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -0,0 +1,25 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ubicacion {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Float latitud;
+    private Float longitud;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Float getLatitud() { return latitud; }
+    public void setLatitud(Float latitud) { this.latitud = latitud; }
+
+    public Float getLongitud() { return longitud; }
+
+    public void setLongitud(Float longitud) { this.longitud = longitud; }
+}
